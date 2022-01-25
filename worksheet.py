5032b589d38baee9e0566209ed8815697591d5e1
Curran McConnell <curran.mcconnell@protonmail.com>
2022-01-25 18:53:58 -0500

    start draft of worksheet

--- worksheet.py
+++ worksheet.py
@@ -1,2 +1,12 @@
 maximum_algorithms = []
 fibonacci_algorithms = []
+
+list1 = [1,3,7,11,41,3]
+
+def find_max(list1):
+    for i in list1:
+        for j in list1:
+            print(i, j)
+
+if __name__ == '__main__':
+    find_max(list1)
