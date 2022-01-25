e6e99941056f960f9b5aade4b03e4b3fea6262c6
Curran McConnell <curran.mcconnell@protonmail.com>
2022-01-25 18:13:42 -0500

    remove best_max from worksheet

--- worksheet.py
+++ worksheet.py
@@ -1,9 +1,2 @@
-def best_max(l):
-    result = l[0]
-    for entry in l:
-        if entry > result:
-            result = entry
-    return result
-
 maximum_algorithms = [best_max]
 fibonacci_algorithms = []
